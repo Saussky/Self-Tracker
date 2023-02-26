e0cd87a0c9b7059dc1a001cf1dc5b15f57ee9a91
Saucy <patrickgardnerbrunton@hotmail.com>
2023-02-27 08:26:29 +1100

    removed login check to use the middleware function instead

--- src/pages/api/middleware/jwt.ts
+++ src/pages/api/middleware/jwt.ts
@@ -1,5 +1,6 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
+import users from "../../../../lib/db/authentication/users";
 
 const jwtSecret = process.env.JWT_SECRET ? process.env.JWT_SECRET : 'moon';
 
@@ -23,10 +24,15 @@ export const jwtMiddleware = (handler: NextApiHandler) => async (req: NextApiReq
         const decodedToken = jwt.verify(token, jwtSecret) as JwtPayload;
         req.user = decodedToken;
 
+        const result = await users.getUser(req.user.email);
+        if (result.rows.length === 0) {
+            throw new Error("Invalid user email");
+        }
+
         await handler(req, res);
     } catch (e) {
         console.log(e)
         console.log("Middleware failed")
-        res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ message: "Unauthorized" });
     }
 };
